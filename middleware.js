ad772365986566eaea6989fd93a3f31ff1039185
Alexander0013 <alex870102@gmail.com>
2023-11-06 10:02:19 -0800

    finish up image upload and thumbnail virtual

--- middleware.js
+++ middleware.js
@@ -33,6 +33,7 @@ module.exports.isReviewAuthor = async (req, res, next) => {
 
 module.exports.validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
+  console.log(req.body);
   if (error) {
     const msg = error.details.map((el) => el.message).join(",");
     throw new ExpressError(msg, 400);
